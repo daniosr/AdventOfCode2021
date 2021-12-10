9e6d5c194439ad27b691d00bda597df2c0b0a050
daniosr <65135259+daniosr@users.noreply.github.com>
2021-12-10 01:59:36 +0000

    Day 1 Part 1
    Completed problem 1 of Day 1 of Advent of code

---
+++ Day1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Get the data from the internet
+System.Net.WebClient wc = new System.Net.WebClient();
+
+// Sets the cookie information so correct data can be fetched
+wc.Headers.Add(System.Net.HttpRequestHeader.Cookie, "session=53616c7465645f5f4b727ee5d0bc8441103fd0afc7551804037f39e2f6709bf183efb0c3cb68f25c5cca3edb319cb64b");
+string raw = wc.DownloadString("https://adventofcode.com/2021/day/1/input");
+
+// splits the array into lines
+string[] rawArray = raw.Split("\n");
+
+int count = 0;
+int previous = int.MaxValue;
+int current = 0;
+
+bool success;
+
+// Goes through each lines and calculates the count
+foreach (string line in rawArray)
+{
+    success = Int32.TryParse(line, out current);
+
+    if(success && current > previous)
+    {
+        count++;
+    } 
+    
+    if(success)
+    {
+        previous = current;
+    }
+}
+
+Console.WriteLine(count);
